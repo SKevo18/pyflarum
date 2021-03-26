2f992bb479f144c1dc8d487b76d5603c7f3d8072
Kevo <customworldcompany@gmail.com>
2021-03-26 17:25:15 +0100

    Fix invalid classifier

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9.1',
+        'Programming Language :: Python :: 3.9',
     ],
 )
