8f96eab896aec01dac13b720409de2e1dba5a6ab
Kevo <me@kevo.link>
2021-07-14 11:29:12 +0200

    Upload test

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -8,9 +8,10 @@ import os
 import requests
 
 from pyflarum import FlarumUser
+from pyflarum.extensions.admin import AdminFlarumUserMixin, AdminExtension
 
 
-user = FlarumUser(forum_url=os.environ['forum_url'], username='test', password=os.environ['account_password'])
+user = FlarumUser(forum_url=os.environ['forum_url'], username='test', password=os.environ['account_password'], extensions=[AdminExtension]) # type: AdminFlarumUserMixin
 
 
 if __name__ == "__main__":
@@ -18,6 +19,13 @@ if __name__ == "__main__":
     birb_link = requests.get("https://some-random-api.ml/img/birb").json()['link']
     birb_image = requests.get(birb_link, stream=True).content
 
-    # Upload it as avatar
-    my_user = user.upload_avatar(birb_image, file_type="image/jpeg")
+    # Upload it as avatar:
+    my_user = user.upload_avatar(birb_image)
     print(my_user.avatarUrl)
+
+    # Also, why not upload it as logo?
+    user.upload_logo(birb_image)
+
+    # Remove uploaded favicon:
+    user.remove_favicon()
+    print("OK")
