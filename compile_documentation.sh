202fe34276839af616c5a162bde950e288af00a1
Kevo <me@kevo.link>
2021-07-18 11:18:26 +0200

    Fix docs?

--- compile_documentation.sh
+++ compile_documentation.sh
@@ -2,4 +2,5 @@
 cd "$(dirname "$0")"
 
 pdoc --force --html pyflarum --output-dir ./documentation --template-dir ./documentation/templates
-mv ./documentation/pyflarum ./documentation/docs
+cp ./documentation/pyflarum ./documentation/docs
+rm -r ./documentation/pyflarum
