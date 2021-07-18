c0f3786b4feb48ac70daf2cd31fd565213f447c7
Kevo <me@kevo.link>
2021-07-18 11:20:03 +0200

    -r option

--- compile_documentation.sh
+++ compile_documentation.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 
 pdoc --force --html pyflarum --output-dir ./documentation --template-dir ./documentation/templates
-cp ./documentation/pyflarum ./documentation/docs
+cp -r ./documentation/pyflarum ./documentation/docs
 rm -r ./documentation/pyflarum
