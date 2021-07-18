3397e97073348d0a21d1ac27ea16aa7860ec9b10
Kevo <me@kevo.link>
2021-07-18 11:46:00 +0200

    Remove two newlines

--- compile_documentation.sh
+++ compile_documentation.sh
@@ -4,4 +4,3 @@ cd "$(dirname "$0")"
 pdoc --force --html pyflarum --output-dir documentation --template-dir documentation/templates
 mkdir -p ./documentation/docs
 cp -r ./documentation/pyflarum/* ./documentation/docs && rm -R ./documentation/pyflarum
-
