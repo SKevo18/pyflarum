214748b9539f961f521cfb8a86805fcd1383f108
Kevo <me@kevo.link>
2021-09-12 10:31:44 +0200

    Untested stuff

--- pyflarum/database/session.py
+++ pyflarum/database/session.py
@@ -1,11 +1,20 @@
+import typing as t
+
 from peewee import *
+
+from .models.discussions import DB_Discussion
+from .models.posts import DB_Post
+
+
+MODELS = {DB_Discussion, DB_Post} # type: t.Set[Model]
 
 
 
 class FlarumDatabase:
-    def __init__(self, database: Database):
+    def __init__(self, database: Database, extensions: t.Optional[t.Iterable]=None):
         self.database = database
 
 
-    def get_discussions():
-        pass
+    def get_discussions(self, *fields) -> t.Iterator[DB_Discussion]:
+        with self.database as database:
+            return (DB_Discussion.select(*fields).execute(database))
